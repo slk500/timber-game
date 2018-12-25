b74b9477010004365555b312a649d30fb19c9aa5
Sławomir Grochowski <slawomir.grochowski@exorigo-upos.pl>
2018-12-25 23:08:34 +0100

    add run script, black screen

--- main.cpp
+++ main.cpp
@@ -1,24 +1,12 @@
 #include <SFML/Graphics.hpp>
 
+using namespace sf;
+
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+   VideoMode vm(1920, 1080);
 
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
+   RenderWindow window(vm, "Timber Game", Style::Fullscreen);
 
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
-
-    return 0;
+   return 0;  
 }
