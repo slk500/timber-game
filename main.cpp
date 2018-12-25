ee69f7ddc14358263c3ca2596ad551cb9fc9538b
slk500 <slawomir.grochowski@exorigo-upos.pl>
2018-12-26 00:24:42 +0100

    sketch game loop

--- main.cpp
+++ main.cpp
@@ -6,7 +6,23 @@ int main()
 {
    VideoMode vm(1920, 1080);
 
-   RenderWindow window(vm, "Timber Game", Style::Fullscreen);
+   RenderWindow window(vm, "Timber Game", Style::Fullscreen);   
+   
+   while(window.isOpen())
+     {
+       //handle player input
+       if (Keyboard::isKeyPressed(Keyboard::Escape)) {
+	 window.close();
+       }
 
+       //update the scene
+
+       //clear previous frame
+       window.clear();
+
+       //draw new frame
+       window.display();
+     }
+   
    return 0;  
 }
