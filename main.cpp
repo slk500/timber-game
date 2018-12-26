b666b417f6b52a88771080e78ec8be87f2c24d70
slk500 <slawomir.grochowski@gmail.com>
2018-12-26 14:34:31 +0100

    bee is flying

--- main.cpp
+++ main.cpp
@@ -1,40 +1,85 @@
 #include <SFML/Graphics.hpp>
+#include <experimental/random> 
 
 using namespace sf;
 
+  VideoMode vm(1920, 1080);
+  RenderWindow window(vm, "Timber Game", Style::Fullscreen);   
+
+void exit_game_on_kbd_escape(){
+
+  if (Keyboard::isKeyPressed(Keyboard::Escape)) {
+    window.close();
+  }
+  
+}
+
 int main()
 {
-   VideoMode vm(1920, 1080);
+  Texture textureBackground;
+  textureBackground.loadFromFile("assets/graphics/background.png");
 
-   RenderWindow window(vm, "Timber Game", Style::Fullscreen);   
+  Sprite spriteBackground;
+  spriteBackground.setTexture(textureBackground);
+  spriteBackground.setPosition(0,0);
 
-   Texture textureBackground;
-   textureBackground.loadFromFile("assets/graphics/background.png");
+  //tree
+  Texture textureTree;
+  textureTree.loadFromFile("assets/graphics/tree.png");
+  Sprite spriteTree;
+  spriteTree.setTexture(textureTree);
+  spriteTree.setPosition(810,0);
 
-   Sprite spriteBackground;
-   spriteBackground.setTexture(textureBackground);
-
-   spriteBackground.setPosition(0,0);
-
+  //bee
+  Texture textureBee;
+  textureBee.loadFromFile("assets/graphics/bee.png");
+  Sprite spriteBee;
+  spriteBee.setTexture(textureBee);
+  bool beeActive = false;
    
-   while(window.isOpen())
-     {
-       //handle player input
-       if (Keyboard::isKeyPressed(Keyboard::Escape)) {
-	 window.close();
-       }
+  //clouds
+  Texture textureCloud;
+  textureCloud.loadFromFile("assets/graphics/cloud.png");
+  Sprite spriteCloud;
+  spriteCloud.setTexture(textureCloud);
+  spriteCloud.setPosition(0,250);
 
-       //update the scene
+  int off_the_screen_position_x = 2000;
+  int bee_y_position = 250;
+  spriteBee.setPosition(off_the_screen_position_x, bee_y_position);
+  
+  bool cloudActive = false;
+  float cloudSpeed = 0.0f;
+   
+  Clock clock;
+   
+  while(window.isOpen())
+    {
+      exit_game_on_kbd_escape();
+	
+      Time dt = clock.restart();
 
-       //clear previous frame
-       window.clear();
+      int beeSpeed = std::experimental::randint(200,400); 
+      int bee_x_position = spriteBee.getPosition().x - (beeSpeed * dt.asSeconds());
+      spriteBee.setPosition(bee_x_position, bee_y_position);	 
+      if (spriteBee.getPosition().x < 1){
+	spriteBee.setPosition(2000, 250);
+      }
 
-       //draw game scene
-       window.draw(spriteBackground);
+      
+      //clear previous frame
+      window.clear();
+
+      //draw game scene
+      window.draw(spriteBackground);
+
+      window.draw(spriteCloud);
+      window.draw(spriteTree);
+      window.draw(spriteBee);
        
-       //draw new frame
-       window.display();
-     }
+      //draw new frame
+      window.display();
+    }
    
-   return 0;  
+  return 0;  
 }
--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-g++ -c main.cpp && 
-g++ main.o -o sfml-app -lsfml-graphics -lsfml-window -lsfml-system &&
+g++-8 -c main.cpp && 
+g++-8 main.o -o sfml-app -lsfml-graphics -lsfml-window -lsfml-system &&
 ./sfml-app
